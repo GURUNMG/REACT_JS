a24aef0ea99deb6fc39c27025afddbf29cc17aa8
GURUBHARAN N M <gurubharan.cs20@bitsathy.ac.in>
2022-11-12 12:06:58 +0530

    functional component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
+import React ,{Components} from 'react';
 import './App.css';
+
+// functional components
+import Hello from "./components/Hello"
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
+      <h1>...........................</h1>
+      <h1 class="not-italic hover:italic  text-2xl hover:text-6xl ">
+        Hello GURU
       </h1>
+      <h1>...........................</h1>
+      <Hello></Hello>
+      
     </div>
   );
 }
---
+++ src/components/Hello.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// function Hello()
+// {
+ // return <h1 className=' text-sky-400/75 hover:italic  '>
+ //         Heloo is functional component
+ //        </h1>
+// }
+
+ const Hello=()=>
+{
+ return <h1 className=' text-sky-400/75 hover:italic  '>
+         Heloo is functional component
+        </h1>
+}
+
+export default Hello;
