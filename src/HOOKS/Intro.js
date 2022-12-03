d9670381297adab220417235d29b283bac5fa715
GURUBHARAN N M <gurubharan.cs20@bitsathy.ac.in>
2022-12-03 09:02:12 +0530

    HOOKS condition

--- src/HOOKS/Intro.js
+++ src/HOOKS/Intro.js
@@ -9,11 +9,17 @@ function Update()
     <h2 class="text-red-500 font-bold">HOOKS STARTS...</h2>
     <h1>My name is {name}</h1>
 
-    <button class=" mx-3 border-2  border-orange-400" type="button" onClick={()=>setName("N M G")}>NMG</button>
-    <button class="mx-3 border-2  border-orange-400" type="button" onClick={()=>setName("GURU")}>GURU</button>
-    <button class="mx-3 border-2  border-orange-400" type="button" onClick={()=>setName("BHARAN")}>BHARAN</button>
+    <button class=" mx-3 border-2  border-orange-400" type="button" 
+        onClick={()=>setName("N M G")}>NMG</button>
+    <button class="mx-3 border-2  border-orange-400" type="button" 
+        onClick={()=>setName("GURU")}>GURU</button>
+    <button class="mx-3 border-2  border-orange-400" type="button"
+        onClick={()=>setName("BHARAN")}>BHARAN</button>
   </div>
  );
 }
 
+
+// HOOKS CAN BE USED ONLY IN FUNCTIONAL COMPONENTS..
+// HOOKS CANNOT BE CONDITIONAL
 export default Update;
